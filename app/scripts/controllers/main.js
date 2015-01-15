a9c9a1bcaf6b019a609f92af1939818642002583
Michael De Abreu <michaeljota@users.noreply.github.com>
2015-01-15 17:45:05 -0430

    Yeoman Angular generator.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arheadosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the arheadosApp
+ */
+angular.module('arheadosApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
